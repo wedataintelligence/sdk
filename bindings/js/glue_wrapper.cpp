8ed95b1685c4cba2c71c10fb4d1bc940849b7dd7
Javier Serrano <js@mega.co.nz>
2015-11-23 02:34:49 +0100

    Fixed all enums
    - Fixed the code generator to support enums inside classes inside a
    namespace
    - Modified the idl file
    - Fixed the compilation defining enum types in glue_wrapper.cpp

--- bindings/js/glue_wrapper.cpp
+++ bindings/js/glue_wrapper.cpp
@@ -1,3 +1,26 @@
 #include <megaapi.h>
+
+//Fix enums
+typedef int ProxyType;
+typedef int NodeType;
+typedef int NodeChangeType;
+typedef int UserVisibility;
+typedef int UserChangeType;
+typedef int ShareAccessType;
+typedef int RequestType;
+typedef int TransferType;
+typedef int ContactRequestState;
+typedef int ContactRequestReplyType;
+typedef int ContactRequestAction;
+typedef int ErrorType;
+typedef int LogLevel;
+typedef int FileAttrType;
+typedef int UserAttrType;
+typedef int PaymentMethod;
+typedef int TransferMethod;
+typedef int SortOrder;
+typedef int AccountType;
+typedef int SubscriptionStatus;
+
 #include "glue.cpp"
 
