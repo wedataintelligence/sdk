966066a0eb56cbe0a2050f78f3126620f5b7bf40
Diego Casorran <dc@mega.co.nz>
2015-11-25 19:08:52 +0100

    Adding a bunch of terminal commands.

--- bindings/js/postfix.js
+++ bindings/js/postfix.js
@@ -1,2 +1,20 @@
-return Module;
+    Module.isMyObject = function isMyObject(obj) {
+        return (obj && typeof obj === 'object' && typeof obj.ptr === 'number');
+    };
+    Module.isNULL = function isNULL(obj) {
+        return !Module.isMyObject(obj) || obj.ptr === 0;
+    };
+    Module.isMegaList = function isMegaList(obj) {
+        return !Module.isNULL(obj) && typeof obj.size === 'function' && typeof obj.get === 'function';
+    };
+    Module.getMegaList = function getMegaList(aMegaList) {
+        var list = [];
+        if (Module.isMegaList(aMegaList)) {
+            for (var len = aMegaList.size(), i = 0 ; i < len ; ++i ) {
+                list.push(aMegaList.get(i));
+            }
+        }
+        return list;
+    };
+    return Module;
 }));
--- examples/js/term/term.js
+++ examples/js/term/term.js
@@ -12,10 +12,34 @@ function MegaTerminalListener(aClient, aTerm, aCallback) {
         console.debug('onRequestUpdate', arguments, request.getRequestString());
     };
     listener.onNodesUpdate = function(api, nodes) {
-        console.debug('onNodesUpdate', arguments/*, nodes.size(), nodes.get()*/);
+        console.debug('onNodesUpdate', arguments, MEGASDK.getMegaList(nodes));
     };
     listener.onUsersUpdate = function(api, users) {
-        console.debug('onUsersUpdate', arguments);
+        console.debug('onUsersUpdate', arguments, MEGASDK.getMegaList(users));
+    };
+    listener.onRequestTemporaryError = function(api, request, error) {
+        console.debug('onRequestTemporaryError', arguments);
+    };
+    listener.onTransferStart = function(api, transfer) {
+        console.debug('onTransferStart', arguments);
+    };
+    listener.onTransferUpdate = function(api, transfer) {
+        console.debug('onTransferUpdate', arguments);
+    };
+    listener.onTransferTemporaryError = function(api, transfer, error) {
+        console.debug('onTransferTemporaryError', arguments);
+    };
+    listener.onTransferFinish = function(api, transfer, error) {
+        console.debug('onTransferFinish', arguments);
+    };
+    listener.onAccountUpdate = function(api) {
+        console.debug('onAccountUpdate', arguments);
+    };
+    listener.onReloadNeeded = function(api) {
+        console.debug('onReloadNeeded', arguments);
+    };
+    listener.onContactRequestsUpdate = function(api, requests) {
+        console.debug('onContactRequestsUpdate', arguments, MEGASDK.getMegaList(requests));
     };
     aClient.addListener(listener);
 
@@ -30,6 +54,144 @@ function MegaTerminalListener(aClient, aTerm, aCallback) {
         aCallback = cb;
     };
 }
+
+var accesslevels = [ "read-only", "read/write", "full access" ];
+function dumptree(api, term, n, recurse, depth)
+{
+    depth = depth || 0;
+
+    console.debug('dumptree', n.getName(), recurse, depth);
+
+    if (depth)
+    {
+        var title = n.getName() || "CRYPTO_ERROR";
+        var line = Array(depth).join("    ") + title + ' (';
+
+        switch (n.getType())
+        {
+            case MEGASDK.MegaNode.TYPE_FILE:
+                line += n.getSize();
+
+                // const char* p;
+                // if ((p = strchr(n->fileattrstring.c_str(), ':')))
+                // {
+                    // line += ", has attributes " << p + 1;
+                // }
+
+                // if (n->plink)
+                if (n.isExported())
+                {
+                    line += ", shared as exported";
+                    // if (n->plink->ets)
+                    // {
+                        // line += " temporal";
+                    // }
+                    // else
+                    // {
+                        // line += " permanent";
+                    // }
+                    line += " file link";
+                }
+                break;
+
+            case MEGASDK.MegaNode.TYPE_FOLDER:
+                line += "folder";
+
+                // if(n->outshares)
+                if (n.isOutShare())
+                {
+                    var shares = api.getOutShares(n);
+                    for (var len = shares.size(), i = 0; i < len ; i++ )
+                    {
+                        var share = shares.get(i);
+
+                        // if (it->first)
+                        {
+                            line += ", shared with " + share.getUser() + ", access "
+                                + accesslevels[share.getAccess()];
+                        }
+                    }
+
+                    if (n.isExported())
+                    {
+                        line += ", shared as exported";
+                        // if (n->plink->ets)
+                        // {
+                            // line += " temporal";
+                        // }
+                        // else
+                        // {
+                            // line += " permanent";
+                        // }
+                        line += " folder link";
+                    }
+                }
+
+                // if (n->pendingshares)
+                // {
+                    // for (share_map::iterator it = n->pendingshares->begin(); it != n->pendingshares->end(); it++)
+                    // {
+                        // if (it->first)
+                        // {
+                            // line += ", shared (still pending) with " << it->second->pcr->targetemail << ", access "
+                                 // << accesslevels[it->second->access];
+                        // }
+                    // }
+                // }
+
+                if (n.isInShare())
+                {
+                    // line += ", inbound " << accesslevels[n->inshare->access] << " share";
+                }
+                break;
+
+            default:
+                line += "unsupported type, please upgrade";
+        }
+
+        line += ")" + (n.hasChanged(MEGASDK.MegaNode.CHANGE_TYPE_REMOVED) ? " (DELETED)" : "");
+        term.echo(line);
+
+        if (!recurse)
+        {
+            return;
+        }
+    }
+
+    if (n.isFolder())
+    {
+        var c = api.getChildren(n, MEGASDK.MegaApi.ORDER_ALPHABETICAL_DESC);
+
+        for (var len = c.size(), i = 0; i < len ; i++ )
+        {
+            dumptree(api, term, c.get(i), recurse, depth + 1);
+        }
+    }
+}
+
+function MegaListener(aListener) {
+    var methods = [
+        "onRequestStart", "onRequestFinish", "onRequestUpdate", "onRequestTemporaryError",
+        "onTransferStart", "onTransferFinish", "onTransferUpdate", "onTransferTemporaryError",
+        "onUsersUpdate", "onNodesUpdate", "onAccountUpdate",
+        "onContactRequestsUpdate", "onReloadNeeded"
+    ];
+
+    var listener = new MEGASDK.MegaListenerInterface();
+
+    for (var method in methods) {
+        if (methods.hasOwnProperty(method)) {
+            method = methods[method];
+            listener[method] = aListener[method] || (function dummyMegaListenerMethod(){});
+        }
+    }
+
+    methods = undefined;
+
+    return listener;
+}
+
+var cwd = false;
 MEGASDK.Terminal = function (client) {
     var $e = $('#terminal').empty();
 
@@ -65,15 +227,7 @@ MEGASDK.Terminal = function (client) {
         });
     }
 
-    var isEMOBJ = function(obj) {
-        return (obj && typeof obj === 'object' && typeof obj.ptr === 'number');
-    };
-    var isNULL = function(obj) {
-        return !isEMOBJ(obj) || obj.ptr === 0;
-    };
-
     var UNIT_TEST = false;
-    var cwd = false;
 
     $e.terminal(function tty(line, term) {
         var argv = $.terminal.parseArguments(line);
@@ -108,7 +262,7 @@ MEGASDK.Terminal = function (client) {
                 type = 'debug';
             }
             console[type].apply(console, arguments);
-            if (type === 'info' || type === 'error') {
+            if (type === 'info' || type === 'error' || type === 'warn') {
                 var color = colors[type];
                 if (color) {
                     var tag = type[0] === 'e' || type[0] === 'w' ? '&#91;!&#93;' : '';
@@ -132,7 +286,11 @@ MEGASDK.Terminal = function (client) {
 
                     switch(type) {
                         case MEGASDK.MegaRequest.TYPE_LOGIN:
+                            localStorage.sid = client.dumpSession();
                             tty('fetchNodes', term);
+                            break;
+                        case MEGASDK.MegaRequest.TYPE_LOGOUT:
+                            delete localStorage.sid;
                             break;
                         case MEGASDK.MegaRequest.TYPE_FETCH_NODES:
                             cwd = client.getRootNode();
@@ -167,17 +325,85 @@ MEGASDK.Terminal = function (client) {
                 }
             });
 
-        var cmd = argv.shift().toLowerCase();
-        if (cmd === 'createfolder' || cmd === 'mkdir') {
+        var cmd = String(argv.shift()).toLowerCase();
+        if (cmd === 'test') {
+            UNIT_TEST = 1;
+            cmd = 'login';
+            argv = '.';
+        }
+        if (cmd === 'login' && String(argv) === '.') {
+            argv = ['jssdk@yopmail.com', 'jssdktest'];
+        }
+        if (cmd === 'mkdir') {
             client.createFolder(argv[0], cwd);
         }
+        else if (cmd === 'version') {
+            term.echo("MEGA SDK version: " + client.getVersion());
+            listener.abort(null, MEGASDK.MegaError.API_OK);
+        }
+        else if (cmd === 'session') {
+            term.echo("Your (secret) session is: " + client.dumpSession());
+            listener.abort(null, MEGASDK.MegaError.API_OK);
+        }
+        else if (cmd === 'pwd') {
+            term.echo(client.getNodePath(cwd));
+            listener.abort(null, MEGASDK.MegaError.API_OK);
+        }
+        else if (cmd === 'login') {
+            if (localStorage.sid) {
+                if (!argv.length || argv[0] === localStorage.mail) {
+                    argv = [ localStorage.sid ];
+                }
+            }
+            if (!UNIT_TEST && client.isLoggedIn()) {
+                term.echo("Already logged in. Please log out first.");
+                listener.abort(null, MEGASDK.MegaError.API_OK);
+            }
+            else if (!argv.length) {
+                term.echo("Type 'help' for assistance.");
+                listener.abort(null, MEGASDK.MegaError.API_OK);
+            }
+            else if (~String(argv[0]).indexOf('@')) {
+                localStorage.mail = argv[0];
+                client.login.apply(client, argv);
+            }
+            else if (~String(argv[0]).indexOf('#')) {
+                term.echo("TODO");
+                listener.abort();
+            }
+            else {
+                term.echo("Resuming session...");
+                client.fastLogin(argv[0]);
+            }
+        }
+        else if (cmd === 'whoami') {
+            client.getUserData(MegaListener({
+                onRequestFinish: function(api, request, error) {
+                    assert(client === api, 'getUserData: Unexpected MegaApi instance');
+                    assert(request.getType() === MEGASDK.MegaRequest.TYPE_GET_USER_DATA,
+                        'getUserData: Request type is not TYPE_GET_USER_DATA');
+
+                    if (error.getErrorCode() === MEGASDK.MegaError.API_OK) {
+                        term.echo("[[;#C6C7C6;]Account Owner:] " + request.getName());
+                        term.echo("[[;#C6C7C6;]Account E-Mail:] " + api.getMyEmail());
+                        term.echo("[[;#C6C7C6;]Account PUBKEY:] " + request.getPassword());
+                        term.echo("[[;#C6C7C6;]Account PRIVKEY:] " + request.getPrivateKey());
+                    }
+                }
+            }));
+        }
         else if (cmd === 'cd') {
-            var e;
-            var node = client.getNodeByPath(argv[0], cwd);
-            if (isNULL(node)) {
+            var node, e;
+            if (!argv.length || argv[0] === '/') {
+                node = client.getRootNode();
+            }
+            else {
+                node = client.getNodeByPath(String(argv[0]), cwd);
+            }
+            if (MEGASDK.isNULL(node)) {
                 assert(false, argv[0] + ': No such file or directory.');
             }
-            else if (node.getType() !== MEGASDK.MegaNode.TYPE_FOLDER) {
+            else if (!node.isFolder()) {
                 assert(false, argv[0] + ': Not a directory.');
             }
             else {
@@ -188,7 +414,7 @@ MEGASDK.Terminal = function (client) {
         }
         else if (cmd === 'rm') {
             var node = client.getNodeByPath(argv[0], cwd);
-            if (isNULL(node)) {
+            if (MEGASDK.isNULL(node)) {
                 assert(false, argv[0] + ': No such file or directory.');
                 listener.abort();
             }
@@ -197,19 +423,30 @@ MEGASDK.Terminal = function (client) {
             }
         }
         else if (cmd === 'ls') {
+            var node;
+            var recursive = (argv[0] === '-R') | 0;
 
+            if (argv.length > recursive) {
+                node = client.getNodeByPath(argv[recursive], cwd);
+            }
+            else {
+                node = cwd;
+            }
+
+            if (MEGASDK.isNULL(node)) {
+                assert(false, argv[recursive] + ': No such file or directory.');
+                listener.abort();
+            }
+            else {
+                dumptree(client, term, node, recursive);
+                listener.abort(null, MEGASDK.MegaError.API_OK);
+            }
         }
         else {
             cmd = apiFuncs[cmd] || cmd;
 
-            if (cmd === 'test') {
-                UNIT_TEST = 1;
-                cmd = 'login';
-                argv = '.';
-            }
-
-            if (cmd === 'login' && String(argv) === '.') {
-                argv = ['jssdk@yopmail.com', 'jssdktest'];
+            if (cmd === 'reload') {
+                cmd = 'fetchNodes';
             }
 
             if (typeof client[cmd] === 'function') {
@@ -229,7 +466,7 @@ MEGASDK.Terminal = function (client) {
             }
             else {
                 if (cmd === 'help' || cmd === 'h') {
-                    term.echo("API Commands:");
+                    term.echo("[[;#51C6ED;]API Functions:]");
                     var line = '';
                     var lineLength = window.innerWidth / 7;
                     apiFuncs._.map(function(fn) {
@@ -240,24 +477,79 @@ MEGASDK.Terminal = function (client) {
                         }
                         line += fn;
                     });
+                    term.echo("");
+                    term.echo("[[;#51C6ED;]TERM Commands:]");
+
+                    var indent = 4;
+                    [
+                        "login email [password]",
+                        "login exportedfolderurl#key",
+                        "login session",
+                        // "begin [ephemeralhandle#ephemeralpw]",
+                        // "signup [email name|confirmationlink]",
+                        // "confirm",
+                        "session",
+                        // "mount",
+                        "ls [-R] [remotepath]",
+                        "cd [remotepath]",
+                        "pwd",
+                        // "lcd [localpath]",
+                        // "import exportedfilelink#key",
+                        // "put localpattern [dstremotepath|dstemail:]",
+                        // "putq [cancelslot]",
+                        // "get remotepath [offset [length]]",
+                        // "get exportedfilelink#key [offset [length]]",
+                        // "getq [cancelslot]",
+                        // "pause [get|put] [hard] [status]",
+                        // "getfa type [path] [cancel]",
+                        "mkdir remotepath",
+                        "rm remotepath",
+                        // "mv srcremotepath dstremotepath",
+                        // "cp srcremotepath dstremotepath|dstemail:",
+                    /*  "sync [localpath dstremotepath|cancelslot]",*/
+                        "export remotepath [expireTime|del]",
+                        "share [remotepath [dstemail [r|rw|full] [origemail]]]",
+                        // "invite dstemail [origemail|del|rmd]",
+                        // "ipc handle a|d|i",
+                        // "showpcr", HOW??
+                        // "users", HOW??
+                        // "getua attrname [email]",
+                        // "putua attrname [del|set string|load file]",
+                        // "putbps [limit|auto|none]",
+                        // "killsession [all|sessionid]", -- TODO: killSession is missing
+                        "whoami",
+                        // "passwd",
+                        // "retry", client->abortbackoff ??
+                        // "recon", client->disconnect ??
+                        "reload",
+                        "logout",
+                        "locallogout",
+                        // "symlink",
+                        "version",
+                        // "debug",
+                        "quit"
+                    ].map(function(ln) {
+                        term.echo('[[;#D5F5B8;]' + Array(indent).join(" ") + termEscape(ln) + ']');
+                    });
                 }
                 else {
                     term.echo("Unknown command: " + (cmd || '(null)'));
                 }
-                listener.abort();
+                listener.abort(null, MEGASDK.MegaError.API_OK);
             }
         }
     }, {
-        greetings : 'Javascript SDK',
+        greetings : BANNER.replace('%%', client.getVersion()),
         name : 'jssdk',
         width : '99%',
-        prompt : 'jssdk> '
+        prompt : '[[;#FFFEDD;]jssdk>] '
     });
 
     var $term = $('.terminal-output');
     $term.css({
         "overflow-y" : "auto",
         "overflow-x" : "hidden",
+        "min-height" : "120px"
     });
     $(document.body).css('overflow', 'hidden');
     $(window).resize(function() {
@@ -272,3 +564,12 @@ MEGASDK.Terminal = function (client) {
         }, 90);
     }).trigger('resize');
 };
+
+var BANNER =
+'       ____.                    _________            .__        __     ________________   ____  __. \n'+
+'      |    _____ ___  ______   /   _____/ ___________|_________/  |_  /   _____\\______ \\ |    |/ _| \n'+
+'      |    \\__  \\\\  \\/ \\__  \\  \\_____  \\_/ ___\\_  __ |  \\____ \\   __\\ \\_____  \\ |    |  \\|      <   \n'+
+'  /\\__|    |/ __ \\\\   / / __ \\_/        \\  \\___|  | \\|  |  |_> |  |   /        \\|    `   |    |  \\  \n'+
+'  \\________(____  /\\_/ (____  /_______  /\\___  |__|  |__|   __/|__|  /_______  /_______  |____|__ \\ \n'+
+'                \\/          \\/        \\/     \\/         |__|                 \\/        \\/        \\/ \n'+
+'                                                                           MEGA SDK version:  %%    \n';
